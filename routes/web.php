4a1086c1d9b20f90fd36b7854ceebcbaaa48cb54
Towanou Luc <towanouluc@gmail.com>
2025-04-24 16:32:21 +0100

    installations non fonctionnelle de tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdmDashboardController;
 use App\Http\Controllers\Api\BilleterieController;
 use App\Http\Controllers\Api\QrCodeController;
 use Illuminate\Support\Facades\Route;
@@ -24,6 +25,27 @@ Route::get('/', function () {
 Route::get('/code', [QrCodeController::class, 'vue'])->name('codeqr.vue');
 Route::post('/codeGenerate', [QrCodeController::class, 'generate'])->name('codeqr');
 
+
+// Route::get('/admin', [AdmDashboardController::class, ]->name(''));
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+Route::get('/well', function () {
+    return view('well');
+})->name('well');
+
+
+Route::get('/admin/events/index', function () {
+    return view('admin.events.index');
+})->name('admin.events.index');
+Route::get('/admin/users/index', function () {
+    return view('admin.users.index');
+})->name('admin.users.index');
+Route::get('/admin/tickets/index', function () {
+    return view('admin.tickets.index');
+})->name('admin.tickets.index');
 // Route::get('/paiement/callback', function () {
 //     return 'Paiement terminé. Vous pouvez fermer la fenêtre.';
 // })->name('paiement.callback');
