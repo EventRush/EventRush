b63d946d8de0dfa2ceecfbf628b70248b671fbb7
Towanou Luc <towanouluc@gmail.com>
2025-05-22 18:46:12 +0100

    Initialisatin Laravel Docker config 14

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,20 +22,30 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+
+     public function boot(): void
     {
         Schema::defaultStringLength(191);
         Scramble::configure()->withDocumentTransformers(function (OpenApi $openApi) {
-             // Forcer l'URL de base ici :
-        $openApi->servers = [
-            new Server(
-                url: 'https://0b2f-2c0f-2a80-3bc-b510-dd1-3fb7-9d2d-9e73.ngrok-free.app/api',
-            ),
-        ];
             $openApi->secure(
                 SecurityScheme::http('bearer')
             );
         });
-
     }
+    // public function boot(): void
+    // {
+    //     Schema::defaultStringLength(191);
+    //     Scramble::configure()->withDocumentTransformers(function (OpenApi $openApi) {
+    //          // Forcer l'URL de base ici :
+    //     $openApi->servers = [
+    //         new Server(
+    //             url: 'https://0b2f-2c0f-2a80-3bc-b510-dd1-3fb7-9d2d-9e73.ngrok-free.app/api',
+    //         ),
+    //     ];
+    //         $openApi->secure(
+    //             SecurityScheme::http('bearer')
+    //         );
+    //     });
+
+    // }
 }
