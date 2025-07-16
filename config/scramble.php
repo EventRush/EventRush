53a3247d8a3fd56adcb16990abbc729f3f06a676
Towanou Luc <towanouluc@gmail.com>
2025-07-16 11:15:15 +0100

    modification scramble.php

--- config/scramble.php
+++ config/scramble.php
@@ -89,7 +89,10 @@ return [
      * ],
      * ```
      */
-    'servers' => null,
+    'servers' => [
+         'Live' => 'api',
+         'Prod' => 'http://digitstage.online/api',
+     ],
 
     /**
      * Determines how Scramble stores the descriptions of enum cases.
